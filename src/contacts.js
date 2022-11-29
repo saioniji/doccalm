17e22e0170ac8c90882705ad0427ebc41b6f9668
saioniji <119358131+saioniji@users.noreply.github.com>
2022-11-29 22:14:48 +0400

    Add files via upload

---
+++ src/contacts.js
@@ -0,0 +1,10 @@
+const contacts = [
+    ['Татьяна Владимировна Чапала' + '\n' + 'https://vk.com/id625482513' + '\n' + '89371837900'],
+    ['Мария Илич' + '\n' + 'https://vk.com/ilich.mariya' + '\n' + '+77057733086'],
+    ['Юлия Петрова' + '\n' + 'https://vk.com/id6037251'],
+    ['Оксана Зотова' + '\n' + 'https://vk.com/id128316097'],
+    ['Алина Гельметдинова' + '\n' + 'https://vk.com/id73431394'],
+    ['Юлия Галимова' + '\n' + 'https://vk.com/blon_jul']
+];
+
+module.exports = contacts
